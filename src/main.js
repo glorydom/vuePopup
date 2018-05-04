4563d618f00e2a076bc5bed8cd6d5d9b81cdc34e
huihui.zhang <glorydom@163.com>
2018-05-04 12:32:49 +0800

    popup

--- src/main.js
+++ src/main.js
@@ -2,17 +2,20 @@ import Vue from 'vue'
 import App from './App.vue'
 import axios from 'axios'
 
-import RecurrenceSetting from './components/RecurrenceSetting.vue'
-
-import ElementUI from 'element-ui'
+import popup from './components/popup.vue'
+import elmentUI from 'element-ui'
+// import  { Button, MessageBox, Input } from 'element-ui'
 //import 'element-ui/lib/theme-chalk/index.css'
 
 
-Vue.use(ElementUI)
+// Vue.use(Button)
+// Vue.use(MessageBox)
+// Vue.use(Input)
+Vue.use(elmentUI)
 Vue.prototype.$http = axios
 
 
 new Vue({
   el: '#app',
-  render: h => h(RecurrenceSetting)
+  render: h => h(popup)
 })
